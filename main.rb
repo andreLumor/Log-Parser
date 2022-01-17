a954e47f66b9ef55c6c39faa830509f00e0624aa
André Moreira <andre.moreira@codeminer42.com>
2022-01-17 15:37:25 -0300

    Refactoring main, now object oriented

--- main.rb
+++ main.rb
@@ -1,3 +1,10 @@
-log = File.open('logs/games.log')
-puts log.readlines.map(&:chomp)[0]
+require_relative 'src/log_parser.rb'
+
+unless ARGV.empty?
+    file_path = ARGV[0]
+else
+    raise "Must give file path as parameter"
+end
+log = Log_Parser.new(file_path)
+log.print_lines(1)
 log.close
