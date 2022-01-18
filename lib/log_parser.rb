4c13ac180e47c9b109bebc3488e40aeccda31735
André Moreira <andre.moreira@codeminer42.com>
2022-01-18 17:52:13 -0300

    even more identation corrections

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,16 +1,16 @@
 class LogParser
-	
-	def initialize(file_path)
-		if File.file?(file_path)
-			@file_path = file_path
-		else
-			raise "File not found"
-		end		
-	end
+  
+  def initialize(file_path)
+    if File.file?(file_path)
+      @file_path = file_path
+    else
+      raise "File not found"
+    end		
+  end
 
-	def head
-		File.open(@file_path, 'r') do |file|
-			file.first 
-		end 
-	end
+  def head
+    File.open(@file_path, 'r') do |file|
+      file.first 
+    end 
+  end
 end
