57c0b584040c942578f0b3ed64084900fb377df3
André Moreira <andre.moreira@codeminer42.com>
2022-01-18 14:39:33 -0300

    changing identation to 2 spaces

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 require_relative 'lib/log_parser.rb'
 
 unless ARGV.empty?
-    file_path = ARGV[0]
+	file_path = ARGV[0]
 else
-    raise "Must give file path as parameter"
+	raise "Must give file path as parameter"
 end
 parser = Log_Parser.new(file_path)
 puts parser.head
