caf530d75a6fdaa87a01541120036ee3670e5795
André Moreira <andre.moreira@codeminer42.com>
2022-01-18 10:27:45 -0300

    correcting rspec tests

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/log_parser'
 describe Log_Parser do
     it 'Returns the first line of the file when the method Log_Parser.lines is called' do
         l = Log_Parser.new('spec/test')
-        expect(l.head).to be == "line 1"
+        expect(l.head).to be == "line 1\n"
     end
 
     it 'Raises "File not found" error when invalid path is given as argument' do
