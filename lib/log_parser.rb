8fd6977ab49104e1bc0cb49ca8518f49ab467778
André Moreira <andre.moreira@codeminer42.com>
2022-01-19 15:27:12 -0300

    Creating LogParser.kills and LogParser.total_kills
    Defining methods and updating main.rb output

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -29,6 +29,7 @@ class LogParser
     @score.keys
   end
 
+<<<<<<< HEAD
   def get_json
     json_info = {
       "lines": self.count, 
@@ -36,6 +37,8 @@ class LogParser
     }
   end
 
+=======
+>>>>>>> Creating LogParser.kills and LogParser.total_kills
   #return kills hash
   def kills
     @kills
