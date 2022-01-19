42e5f4cb52ea1bb4adfd809cf3cfe637f018bf81
André Moreira <andre.moreira@codeminer42.com>
2022-01-19 12:11:50 -0300

    Creating test for LogParser.count and changing test file
    Changed test file to dummy.log with a more representative example of the original file

--- spec/lib/log_parser_spec.rb
+++ spec/lib/log_parser_spec.rb
@@ -9,8 +9,15 @@ describe LogParser do
 
   describe '#head' do
     it 'Returns the first line of the file' do
-        l = LogParser.new('spec/fixtures/test')
-        expect(l.head).to be == "line 1\n"
+        l = LogParser.new('spec/fixtures/dummy.log')
+        expect(l.head).to be == "  1:47 InitGame: \\sv_floodProtect\\1\\sv_maxPing\\0\\sv_minPing\\0\\sv_maxRate\\10000\\sv_minRate\\0\\sv_hostname\\Code Miner Server\\g_gametype\\0\\sv_privateClients\\2\\sv_maxclients\\16\\sv_allowDownload\\0\\bot_minplayers\\0\\dmflags\\0\\fraglimit\\20\\timelimit\\15\\g_maxGameClients\\0\\capturelimit\\8\\version\\ioq3 1.36 linux-x86_64 Apr 12 2009\\protocol\\68\\mapname\\q3dm17\\gamename\\baseq3\\g_needpass\\0\n" 
+    end
+  end
+
+  describe '#count' do
+    it 'Returns number of lines of the file' do
+        l = LogParser.new('spec/fixtures/dummy.log')
+        expect(l.count).to be == 33
     end
   end
 end
