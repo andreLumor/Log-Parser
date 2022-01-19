e89072eac22d388a617ea35c9dde267b771bb89e
André Moreira <andre.moreira@codeminer42.com>
2022-01-19 12:30:01 -0300

    Adding kills parameter and players method to LogParser
    Added a hash that saves all the players and their kills on initialize.
    Added players method that return all the players from the log.

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -5,7 +5,9 @@ class LogParser
       @file_path = file_path
     else
       raise "File not found"
-    end		
+    end
+    
+    @kills = initialize_kills
   end
 
   #retorna a primeira linha do arquivo
@@ -21,4 +23,42 @@ class LogParser
       file.count
     end 
   end
+
+  def players
+    @kills.keys
+  end
+
+  private
+  def initialize_kills
+    kills_hash = {}
+    File.readlines(@file_path).each do |line|
+
+      if line.split[1] == 'Kill:'
+        killer = line.split("killed")[0].split[5..].join(' ')
+        victm = line.split("killed")[1].split[..-3].join(' ')
+        if kills_hash.key?(killer)
+          kills_hash[killer] += 1
+        else
+          kills_hash[killer] = 1
+        end
+
+        if kills_hash.key?(victm)
+          kills_hash[victm] += 1
+        else
+          kills_hash[victm] = 0
+        end
+      end
+    
+      if line.split[1] == 'ClientUserinfoChanged:'
+        player = line.split('\\')[1]
+
+        unless kills_hash.key?(player)
+          kills_hash[player] = 0
+        end
+      end
+    end
+    kills_hash.delete('<world>')
+    kills_hash
+  end
+
 end
--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ end
 
 parser = LogParser.new(file_path)
 
-json_info = {"lines": parser.count}
+json_info = {"lines": parser.count, "players": parser.players}
 
 print file_path+": "
 
