4bbabfed0799a4cd74cfd91575a41de307b73ab4
André Moreira <andre.moreira@codeminer42.com>
2022-01-26 16:46:30 -0300

    fixing main

--- main.rb
+++ main.rb
@@ -9,13 +9,7 @@ end
 
 parser = LogParser.new(file_path)
 
-json_info = {
-  "lines": parser.count, 
-  "players": parser.players, 
-  "kills": parser.kills, 
-  "total_kills": parser.total_kills 
-}
-
+json_info = parser.get_json
 print file_path+": "
 
 puts JSON.pretty_generate(json_info)
